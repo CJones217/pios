295c762166bbab7d92e960f29156e72166ca32ec
Collin Jones <cjones28@luc.edu>
2021-01-30 13:50:23 -0600

    possible finish to list

--- src/list.c
+++ src/list.c
@@ -1,10 +1,12 @@
 /*In list.c, write a pair of functionslistadd()andlistremove()that add and remove elements from a linkedlist*/
+#include<stdlib.h>
 #include "list.h"
 
-void list_add(){
-
+void list_add(struct list_element *f, struct list_element *a, int data){
+    f->next=&a;
+    a->data=data;
 }
 
-void list_remove(){
-
+void list_remove(struct list_element *l){
+    l->next=NULL;
 }
--- src/list.h
+++ src/list.h
@@ -1,2 +1,6 @@
 /*In list.h, define a general purpose struct for a linked list element.*/
 
+struct list_element{
+    struct list_element *next;
+    int data;
+};
