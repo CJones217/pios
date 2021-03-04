d1e576409d9c12c772d391701a39b781fc3859cc
Collin Jones <cjones28@luc.edu>
2021-03-04 14:04:08 -0600

    it compiles but havent tested if it works

--- src/page.c
+++ src/page.c
@@ -18,15 +18,42 @@ void init_pfa_list(){
 }
 
 //too lazy to work on this tonight
-/*struct ppage *allocate_physical_pages(unsigned int npages){ 
-    int i=0;
-    while(){
+struct ppage *allocate_physical_pages(unsigned int npages){ 
+    struct ppage *allocd_list;
+    struct ppage *page_to_break;
+    allocd_list = head;//does this change when I change the head after the forloop?
+    page_to_break = head;
 
+    
+    int i;
+
+    for(i=1;i<npages;i++){
+        page_to_break=page_to_break->next;
     }
-}*/
+
+    head=page_to_break->next;//move head to node after allocd nodes
+    page_to_break->next->prev=NULL; //remove node after allocd nodes connection to last allocd node
+    page_to_break->next=NULL; // remove allocd nodes connection to free pages
+
+    return allocd_list;
+
+
+}
 
 void free_physical_pages(struct ppage *ppage_list){
+    //move to end of page list and connect last node to the head, then point head at first node of this list
+    struct ppage *for_head;
+    struct ppage *add;
+    for_head=ppage_list;
+    add=ppage_list;
 
+    while(add->next!=NULL){
+        add=add->next;
+    }
+    add->next=head;
+    head->prev=add;
+    head=for_head;
+    
 }
 
 void page_list_add(struct ppage *f, struct ppage *a){ //prob not finished
